22209732f22d8c5f948ba9e88719f6a715869647
Steven Hoang <hoangbaoduy@gmail.com>
2024-08-11 17:20:56 +0800

    update

--- SqlDataCleanup/SqlCleanupJob.cs
+++ SqlDataCleanup/SqlCleanupJob.cs
@@ -15,7 +15,7 @@ public class SqlCleanupJob(DbCleanup config)
 
     public async Task RunAsync()
     {
-        Console.WriteLine($"Running SQL Cleanup Job with config:\n {string.Join("\n\t",config.Databases.Keys)}");
+        Console.WriteLine($"Running SQL Cleanup Job with OlderThanDays:{config.OlderThanDays} for dbs:\n {string.Join("\n\t",config.Databases.Keys)}");
 
         var beforeDate = DateTime.Today.AddDays(config.OlderThanDays * -1);
         foreach (var dbConfig in config.Databases)
