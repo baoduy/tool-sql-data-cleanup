1b5d7654ac2989b89cadcb0d4b91d8eaf384500a
Steven Hoang <drunkcoding@outlook.com>
2024-08-12 09:30:53 +0800

    added dependency sort

--- SqlDataCleanup/DbCleanupJob.cs
+++ SqlDataCleanup/DbCleanupJob.cs
@@ -1,13 +1,39 @@
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.InteropServices.Marshalling;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 
 namespace SqlDataCleanup;
 
+class TableDepend
+{
+    [Column("FK_Table")] public string FkTable { get; set; } = default!;
+    [Column("PK_Table")] public string PkTable { get; set; } = default!;
+}
+
 public class TableInfo
 {
+    private readonly HashSet<TableInfo> _listPkTables = [];
+    private int _weight = 0;
+
     [Column("TABLE_NAME")] public string TableName { get; set; } = default!;
     [Column("TABLE_SCHEMA")] public string Schema { get; set; } = default!;
+
+    public int Weaight => _weight;
+    public IEnumerable<TableInfo> PkTables => _listPkTables;
+
+    private void InCreaseWeight()
+    {
+        _weight += 1;
+        foreach (var table in _listPkTables)
+            table.InCreaseWeight();
+    }
+
+    public void AddPkTable(TableInfo table)
+    {
+        if (_listPkTables.Add(table))
+            table.InCreaseWeight();
+    }
 }
 
 public class DbCleanupJob(string name, string connectionString, DateTime beforeDate, DbConfig config)
@@ -30,7 +56,34 @@ public class DbCleanupJob(string name, string connectionString, DateTime beforeD
             !config.ExcludeTables.Contains(s.TableName, StringComparer.InvariantCultureIgnoreCase));
     }
 
-    private string BuildQuery(string table)
+    private async Task<IEnumerable<TableInfo>> SortTableReferences(List<TableInfo> tables)
+    {
+        var tablesDepends = await _context.Database
+            .SqlQuery<TableDepend>($@"
+                SELECT DISTINCT
+    FK_Table = FK.TABLE_NAME, 
+    PK_Table = PK.TABLE_NAME
+FROM 
+    INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS C
+    INNER JOIN INFORMATION_SCHEMA.TABLE_CONSTRAINTS FK
+    ON C.CONSTRAINT_NAME = FK.CONSTRAINT_NAME
+        INNER JOIN INFORMATION_SCHEMA.TABLE_CONSTRAINTS PK
+    ON C.UNIQUE_CONSTRAINT_NAME = PK.CONSTRAINT_NAME")
+            .AsNoTracking().ToListAsync();
+
+        foreach (var tableDepend in tablesDepends)
+        {
+            var pkTable = tables.First(t =>
+                t.TableName.Equals(tableDepend.PkTable, StringComparison.CurrentCultureIgnoreCase));
+            var fkTable = tables.First(t =>
+                t.TableName.Equals(tableDepend.FkTable, StringComparison.CurrentCultureIgnoreCase));
+            fkTable.AddPkTable(pkTable);
+        }
+
+        return tables.OrderBy(t => t.Weaight);
+    }
+
+    private string BuildDeleteQuery(string table)
     {
         var fields = string.Join(" AND ", config.ConditionFields.Select(f => $"{f} < @beforeDate"));
         return $"""
@@ -51,7 +104,8 @@ public class DbCleanupJob(string name, string connectionString, DateTime beforeD
         var hasMoreRows = true;
         while (hasMoreRows)
         {
-            var rowsAffected = await _context.Database.ExecuteSqlRawAsync(BuildQuery(table), new SqlParameter("@beforeDate", beforeDate));
+            var rowsAffected = await _context.Database.ExecuteSqlRawAsync(BuildDeleteQuery(table),
+                new SqlParameter("@beforeDate", beforeDate));
 
             hasMoreRows = rowsAffected > 0;
             count += rowsAffected;
@@ -70,6 +124,8 @@ public class DbCleanupJob(string name, string connectionString, DateTime beforeD
             throw new ArgumentNullException(nameof(config.ConditionFields));
 
         var tables = await GetTablesAsync();
+        tables = await SortTableReferences(tables.ToList());
+
         foreach (var table in tables)
         {
             try
